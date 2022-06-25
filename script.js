3d0fe7346f583ed9cc99dde729f46a0f6541f969
Anagha N G <61464369+nabhat@users.noreply.github.com>
2022-06-25 22:11:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,65 +1,133 @@
-const container = document.querySelector('.container')
-const seats = document.querySelectorAll('.row .seat:not(.occupied)')
-const count = document.getElementById('count')
-const total = document.getElementById('total')
-const movieSelect = document.getElementById('movie');
+const search = document.getElementById('search'),
+    submit = document.getElementById('submit'),
+    random = document.getElementById('random'),
+    mealsEl = document.getElementById('meals'),
+    single_mealEl = document.getElementById('single-meal'),
+    resultHeading = document.getElementById('result-heading');
 
-let ticketPrice = +movieSelect.value;
+function getRandomMeal() {
 
-populateUI()
+    meals.innerHTML = '';
+    resultHeading.innerHTML = '';
 
-//Save selected movie index and price
-function setMovieData(movieIndex,moviePrice) {
-    localStorage.setItem('selectedMovieIndex',movieIndex);
-    localStorage.setItem('selectedMoviePrice',moviePrice);
+    fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
+        .then(res => res.json())
+        .then(data => {
+            const meal = data.meals[0];
+            addMealToDOM(meal);
+        });
+
 }
 
-//Update total and count
-function updateSelectedCount(){
-    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+function searchMeal(e) {
+    e.preventDefault();
 
-    //copy selected seats into arr
-    //map through array
-    //return a new array indexes
-    const seatsindex = [...selectedSeats].map((seat) =>
-         [...seats].indexOf(seat)
-        )
+    single_mealEl.innerHTML = '';
 
-    //Implementing local storage
-    // localStorage.setItem('name',"alok")
-    localStorage.setItem('selectedSeats',JSON.stringify(seatsindex))
+    const term = search.value;
 
+    if (term.trim()) {
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
 
-    const selectedSeatsCount = selectedSeats.length;
-    count.innerText = selectedSeatsCount;
-    total.innerText = selectedSeatsCount * ticketPrice;
+                resultHeading.innerHTML = `<h2>Search results for '${term}' :</h2>`;
+
+                if (data.meals === null) {
+                    resultHeading.innerHTML = `<h2>No result, try again !</h2>`;
+                }
+                else {
+                    mealsEl.innerHTML = data.meals.map(meal =>
+
+                        `<div class="meal">
+                            <img src="${meal.strMealThumb}" alt="${meal.strMeal}"/>
+                            <div class="meal-info" data-mealID="${meal.idMeal}">
+                                <h3>${meal.strMeal}</h3>
+                            </div> 
+                        </div>
+
+                        `
+                        // data-mealID : format for custom attribute in html5
+                        // data-"whatever-we-want"= ""
+                    )
+                        .join('')
+                }
+            });
+        search.value = '';
+    }
+    else
+        alert("Meal can't be blank");
 }
 
-//Get data from local storage and populate ui
-function populateUI() {
-    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'))
+function getMealByID(mealID) {
 
-    if (selectedSeats !== null && selectedSeats.length>0) {
-        seats.forEach((seat,index) =>{
-            if (selectedSeats.indexOf(index) > -1) {
-                seat.classList.add('selected')
-            }
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
+        .then(res => res.json())
+        .then(data => {
+
+            const meal = data.meals[0];
+            addMealToDOM(meal);
+
         })
-    }
+
 }
 
-//movie select event
-movieSelect.addEventListener('change',e =>{
-    ticketPrice = +e.target.value
-    setMovieData(e.target.selectedIndex,e.target.value)
-    updateSelectedCount();
-})
+function addMealToDOM(meal) {
 
-//seat click event
-container.addEventListener('click',(e)=>{
-    if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
-        e.target.classList.toggle('selected')
+    const ingredients = [];
 
-        updateSelectedCount();
+    for (let i = 1; i <= 20; i++) {
+        if (meal[`strIngredient${i}`]) {
+            ingredients.push(`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`);
+        }
+
+        else
+            break;
     }
-})
+
+    single_mealEl.innerHTML =
+        `
+        <div class="single-meal">
+            <h1>${meal.strMeal}</h1>
+            <img src="${meal.strMealThumb}" alt="${meal.strMeal}"/>
+            <div class="single-meal-info">
+                ${meal.strCategory ? ` <p>Category: ${meal.strCategory}</p> ` : ''}
+                ${meal.strArea ? ` <p>Dish type: ${meal.strArea}</p> ` : ''}
+            </div>
+        
+
+        <div class="main">
+            <p>${meal.strInstructions}</p>
+            <h2>Ingredients</h2>
+            <ul>
+                ${ingredients.map(ing => `<li>${ing}</li>`).join('')}
+            </ul>
+        </div>
+        </div>
+    `
+
+}
+
+// Event listeners
+
+submit.addEventListener('submit', searchMeal);
+
+mealsEl.addEventListener('click', e => {
+
+    const mealInfo = e.path.find(item => {
+        if (item.classList) {
+            return item.classList.contains('meal-info');
+        }
+        else {
+            return false;
+
+        }
+    });
+
+    if (mealInfo) {
+        const mealID = mealInfo.getAttribute('data-mealID');
+        getMealByID(mealID);
+    }
+});
+random.addEventListener('click', getRandomMeal);
